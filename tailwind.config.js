27e496d96ccfdaa516ebaa15dca4bfe9add6d0e2
zuelfiekarar <zuelfiekarar@gmail.com>
2023-09-08 08:24:50 +0700

    Navbar#1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'inter' : 'Inter',
+        'asap' : 'Asap Condensed',
+        'caveat' : 'Caveat',
+        'poppins' : 'Poppins',
+      },
+      colors: {
+        'dasar': '#294263',
+        'biru': '#203271',
+        'ijo_tumbuh' :'#2a9d8f',
+      },
+      backgroundImage: {
+        'gradient': 'linear-gradient(90deg, #F8719D 0%, #FEF08A 100%)',
+      },
+      transformOrigin:{
+        'flip-x': 'scaleX(-1)'
+      },
+    },
   },
   plugins: [],
 }
